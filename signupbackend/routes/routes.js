f6e7b53c526797ed302a4b9f86b654b4e80ed9c2
datpham0412 <tiendat041202@gmail.com>
2024-08-06 16:38:35 +1000

    feat: add signup route for user registration

---
+++ signupbackend/routes/routes.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+const signUpTemplateCopy = require("../models/SignUpModels");
+
+router.post("/signup", (request, response) => {
+  const signedUpUser = new signUpTemplateCopy({
+    fullName: request.body.fullName,
+    userName: request.body.userName,
+    email: request.body.email,
+    password: request.body.password,
+  });
+  signedUpUser
+    .save()
+    .then((data) => {
+      response.json(data);
+    })
+    .catch((error) => {
+      response.json(error);
+    });
+});
+
+module.exports = router;
