b386e6c3908dc978fc197b28e450e71ac6574aca
datpham0412 <tiendat041202@gmail.com>
2024-08-07 14:00:19 +1000

    feat: Enhance server setup with CORS and routes configuration

--- signupbackend/server.js
+++ signupbackend/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const routesUrls = require("./routes/routes");
+const cors = require("cors");
 
 dotenv.config();
 
@@ -19,5 +21,8 @@ async function connectToDatabase() {
 }
 
 connectToDatabase();
-
+// 123213
+app.use(express.json());
+app.use(cors());
+app.use("/app", routesUrls);
 app.listen(4000, () => console.log("Server is up and running"));
