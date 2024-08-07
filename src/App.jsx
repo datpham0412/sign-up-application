805df5d246b1cb7d63712358d20795b70ca9a943
datpham0412 <tiendat041202@gmail.com>
2024-08-07 16:16:46 +1000

    feat: Add form to App component with state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,51 @@
 import React, { Component } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css"
 
 class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            fullName: '',
+            username: '',
+            email: '',
+            password: '',
+        }
+    }
     render() {
-        return ( );
+        return ( 
+            <div>
+                <div className='container'>
+                    <div className="form-div">
+                        <form>
+                            <input type="text" 
+                            placeholder='Full Name'
+                            onChange={this.changeFullName}
+                            value={this.state.fullName}
+                            className='form-control form-group'
+                            />
+
+                            <input type="text" 
+                            placeholder='Username'
+                            onChange={this.changeUsername}
+                            value={this.state.username}
+                            className='form-control form-group'/>
+
+                            <input type="text" 
+                            placeholder='Email'
+                            onChange={this.changeEmail}
+                            value={this.state.email}
+                            className='form-control form-group'/>
+
+                            <input type="text" 
+                            placeholder='Password'
+                            onChange={this.changePassword}
+                            value={this.state.password}
+                            className='form-control form-group'/>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        );
     }
 }
 
