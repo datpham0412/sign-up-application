7dc0abc31d11e50c028221ec1560a571419ad210
datpham0412 <tiendat041202@gmail.com>
2024-08-07 12:01:29 +1000

    feat: Connect application to MongoDB

--- signupbackend/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.post("/signup", (request, response) => {
-  response.send("send");
-});
-
-module.exports = router
--- signupbackend/server.js
+++ signupbackend/server.js
@@ -1,4 +1,23 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+// mongoose.connect(process.env.DATABASE_ACCESS, () =>
+//   console.log("Database connected")
+// );
+
+async function connectToDatabase() {
+  try {
+    await mongoose.connect(process.env.DATABASE_ACCESS, {});
+    console.log("Database connected");
+  } catch {
+    console.error("Database connection error:", error);
+  }
+}
+
+connectToDatabase();
 
 app.listen(4000, () => console.log("Server is up and running"));
