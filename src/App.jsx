bccc521c2101f1b8c77b1bda5f5db71017c73e9c
datpham0412 <tiendat041202@gmail.com>
2024-08-07 17:21:16 +1000

    feat: Send form to the backend server

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css"
+import axios from 'axios'
+import { response } from 'express';
 
 class App extends Component {
     constructor() {
@@ -37,12 +39,30 @@ class App extends Component {
         })
     }
 
+    onSubmit(event){
+        event.preventDefault()
+        const registered = {
+            fullName: this.state.fullName,
+            username: this.state.username,
+            email: this.state.email,
+            password: this.state.password
+        }
+        axios.post('http://localhost:4000/app/signup', registered)
+            .then(response => console.log(response.data))
+        this.setState({
+            fullName: '',
+            username: '',
+            email: '',
+            password: ''
+        })
+    }
+
     render() {
         return ( 
             <div>
                 <div className='container'>
                     <div className="form-div">
-                        <form>
+                        <form onSubmit={this.onSubmit}>
                             <input type="text" 
                             placeholder='Full Name'
                             onChange={this.changeFullName}
