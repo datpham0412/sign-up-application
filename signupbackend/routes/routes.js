ce2861933aac14651d826757ee72b15c67ced7f5
datpham0412 <tiendat041202@gmail.com>
2024-08-08 14:55:25 +1000

    feat: Encrypt the password

--- signupbackend/routes/routes.js
+++ signupbackend/routes/routes.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const signUpTemplateCopy = require("../models/SignUpModels");
+const bcrypt = require("bcrypt");
 
-router.post("/signup", (request, response) => {
+router.post("/signup", async (request, response) => {
+  const saltPassword = await bcrypt.genSalt(10);
+  const securePassword = await bcrypt.hash(request.body.password, saltPassword);
+
   const signedUpUser = new signUpTemplateCopy({
     fullName: request.body.fullName,
     username: request.body.username,
     email: request.body.email,
-    password: request.body.password,
+    password: securePassword,
   });
   signedUpUser
     .save()
