c7218b7255c156829d0dc2ad6f9d10198ffdaf8e
datpham0412 <tiendat041202@gmail.com>
2024-08-08 14:39:39 +1000

    fix: Bind onSubmit event handler in constructor

--- src/App.jsx
+++ src/App.jsx
@@ -15,30 +15,31 @@ class App extends Component {
         this.changeUsername = this.changeUsername.bind(this)
         this.changeEmail = this.changeEmail.bind(this)
         this.changePassword = this.changePassword.bind(this)
+        this.onSubmit = this.onSubmit.bind(this)
     }
 
-    changeFullName(event){
+    changeFullName(event) {
         this.setState({
             fullName: event.target.value
         })
     }
-    changeUsername(event){
+    changeUsername(event) {
         this.setState({
             username: event.target.value
         })
     }
-    changeEmail(event){
+    changeEmail(event) {
         this.setState({
             email: event.target.value
         })
     }
-    changePassword(event){
+    changePassword(event) {
         this.setState({
             password: event.target.value
         })
     }
 
-    onSubmit(event){
+    onSubmit(event) {
         event.preventDefault()
         const registered = {
             fullName: this.state.fullName,
@@ -57,37 +58,37 @@ class App extends Component {
     }
 
     render() {
-        return ( 
+        return (
             <div>
                 <div className='container'>
                     <div className="form-div">
                         <form onSubmit={this.onSubmit}>
-                            <input type="text" 
-                            placeholder='Full Name'
-                            onChange={this.changeFullName}
-                            value={this.state.fullName}
-                            className='form-control form-group'
+                            <input type="text"
+                                placeholder='Full Name'
+                                onChange={this.changeFullName}
+                                value={this.state.fullName}
+                                className='form-control form-group'
                             />
 
-                            <input type="text" 
-                            placeholder='Username'
-                            onChange={this.changeUsername}
-                            value={this.state.username}
-                            className='form-control form-group'/>
+                            <input type="text"
+                                placeholder='Username'
+                                onChange={this.changeUsername}
+                                value={this.state.username}
+                                className='form-control form-group' />
 
-                            <input type="text" 
-                            placeholder='Email'
-                            onChange={this.changeEmail}
-                            value={this.state.email}
-                            className='form-control form-group'/>
+                            <input type="text"
+                                placeholder='Email'
+                                onChange={this.changeEmail}
+                                value={this.state.email}
+                                className='form-control form-group' />
 
-                            <input type="text" 
-                            placeholder='Password'
-                            onChange={this.changePassword}
-                            value={this.state.password}
-                            className='form-control form-group'/>
+                            <input type="text"
+                                placeholder='Password'
+                                onChange={this.changePassword}
+                                value={this.state.password}
+                                className='form-control form-group' />
 
-                            <input type="submit" className='btn btn-danger btn-block' value='Submit'/>
+                            <input type="submit" className='btn btn-danger btn-block' value='Submit' />
                         </form>
                     </div>
                 </div>
