b88c1ec1af4941aadaecadd2ea4e2b87420f130e
datpham0412 <tiendat041202@gmail.com>
2024-11-19 00:00:00 +1100

    add

--- signupbackend/routes/routes.js
+++ signupbackend/routes/routes.js
@@ -7,6 +7,7 @@ router.post("/signup", async (request, response) => {
   const saltPassword = await bcrypt.genSalt(10);
   const securePassword = await bcrypt.hash(request.body.password, saltPassword);
 
+  
   const signedUpUser = new signUpTemplateCopy({
     fullName: request.body.fullName,
     username: request.body.username,
